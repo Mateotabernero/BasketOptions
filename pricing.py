0f6fc5d9794685b6e511078ef484fc8a2fa0bd20
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-08-22 20:31:54 +0200

    Update pricing.py

--- pricing.py
+++ pricing.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import math 
-from main import * 
+import main 
 
 
 # This prices basket options given the weights of assets 
@@ -10,7 +10,7 @@ def price (number_of_assets, weights, initial_values, num_steps, T, means, std_d
     values = np.zeros(num_simulations) 
     
     for i in range(num_simulations):
-        S = GBM(number_of_assets, initial_values, num_steps, T, means, std_devs, cov_matrix) 
+        S = main.GBM(number_of_assets, initial_values, num_steps, T, means, std_devs, cov_matrix) 
         values[i] = np.dot(vPayOff(S[:, num_steps]), weights) 
      
     V = np.mean(values)
