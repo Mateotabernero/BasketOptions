bd24f0919b2e37c0be9b90f8a57a9ce58927bd5b
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-08-22 20:31:27 +0200

    Create pricing.py

---
+++ pricing.py
@@ -0,0 +1,18 @@
+import numpy as np 
+import math 
+from main import * 
+
+
+# This prices basket options given the weights of assets 
+def price (number_of_assets, weights, initial_values, num_steps, T, means, std_devs, cov_matrix, payOff, num_simulations = 10000): 
+    vPayOff = np.vectorize(payOff) 
+
+    values = np.zeros(num_simulations) 
+    
+    for i in range(num_simulations):
+        S = GBM(number_of_assets, initial_values, num_steps, T, means, std_devs, cov_matrix) 
+        values[i] = np.dot(vPayOff(S[:, num_steps]), weights) 
+     
+    V = np.mean(values)
+
+    
