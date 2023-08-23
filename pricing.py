678a09e92846576fe1dd18f89a0f19f1ea5b8435
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-08-24 00:06:58 +0200

    Update pricing.py

--- pricing.py
+++ pricing.py
@@ -3,7 +3,17 @@ import math
 import main 
 
 
-# This prices basket options given the weights of assets 
+def GBM(number_of_assets, initial_values, num_steps, T, means, std_devs, cov_matrix): 
+    delta_t = T/num_steps 
+    S = np.ones((number_of_assets, num_steps +1)) 
+    S[:,0] = initial_values
+    
+    W = np.random.multivariate_normal(np.zeros(number_of_assets), cov_matrix, num_steps) 
+    for i in range(num_steps): 
+        S[:,i+1] = S[:,i] + means * S[:, i] * delta_t + std_devs * S[:,i]*math.sqrt(delta_t)*W[i,:]
+    return S 
+    
+
 def price (number_of_assets, weights, initial_values, num_steps, T, means, std_devs, cov_matrix, payOff, num_simulations = 10000): 
     vPayOff = np.vectorize(payOff) 
 
